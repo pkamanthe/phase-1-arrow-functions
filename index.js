516b6170459e501214af54ac6e3ccfe93bc4c3f1
pkamanthe <patriciamumbua97@gmail.com>
2024-09-25 11:39:39 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1. Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+};
+
+// 3. Arrow function called square
+const square = (x) => {
+    return x * x;
+};
+
+// 5. Arrow function called add
+const add = (a, b) => {
+    return a + b;
+};
